5661bfe16df1d870a5c9bbe48aa272178fe0689c
a8yy <xiao17788@outlook.com>
2025-05-27 20:18:51 +0800

    Update douban.js

--- js/douban.js
+++ js/douban.js
@@ -181,7 +181,7 @@ function fillAndSearch(title) {
                 `/s=${encodedQuery}`
             );
             // 更新页面标题
-            document.title = `搜索: ${safeTitle} - LibreTV`;
+            document.title = `搜索: ${safeTitle} - 6080新视觉影院`;
         } catch (e) {
             console.error('更新浏览器历史失败:', e);
         }
